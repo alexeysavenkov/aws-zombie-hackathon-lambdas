d37e874c0b38c40e49eaf12545e9a28ebe88cee5
Alexey Savenkov <alexey199753@gmail.com>
2017-05-28 00:44:19 +0300

    contacts search fixed

--- controllerContacts.js
+++ controllerContacts.js
@@ -5,19 +5,54 @@ import { success, failure } from './libs/response-lib';
 export async function search(event, context, callback) {
 
   var phone = event.pathParameters['query']
-  var lowerUsername = event.pathParameters['query']
-  var upperUsername = lowerUsername.substr(0, lowerUsername.length - 1) + String.fromCharCode(lowerUsername.charCodeAt(lowerUsername.length-1)+1)
+  var username = event.pathParameters['query']
 
-  console.log('kek', phone, lowerUsername, upperUsername)
+  console.log('kek', phone, username)
 
   const userParams = {
       TableName : "User",
       ProjectionExpression:"id, phone, username, image",
-      FilterExpression: "phone = :phone or (username between :lowerUsername and :upperUsername)",
+      FilterExpression: "phone = :phone or contains(username, :username)",
       ExpressionAttributeValues: {
           ":phone": phone,
-          ":lowerUsername": lowerUsername,
-          ":upperUsername": upperUsername
+          ":username": username
+      }
+  };
+
+  try {
+    const userResult = await dynamoDbLib.call('scan', userParams);
+    console.log('userResult', userResult)
+    const users = userResult.Items
+    console.log('users', users)
+    if (users) {
+      // Return the retrieved item
+      callback(null, success(users));
+    }
+    else {
+      callback(null, failure({status: false, error: 'Item not found.'}));
+    }
+  }
+  catch(e) {
+    callback(null, failure({status: false}));
+  }
+}
+
+export async function add(event, context, callback) {
+
+  var userId = event.request.userAttributes.sub
+
+  var phone = event.pathParameters['query']
+  var username = event.pathParameters['query']
+
+  console.log('kek', phone, username)
+
+  const userParams = {
+      TableName : "User",
+      ProjectionExpression:"id, phone, username, image",
+      FilterExpression: "phone = :phone or contains(username, :username)",
+      ExpressionAttributeValues: {
+          ":phone": phone,
+          ":username": username
       }
   };
 
